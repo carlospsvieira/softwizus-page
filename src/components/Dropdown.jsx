903ec846b40ebc7392bef43a81217b436f58af79
Carlos Vieira <carlepsvieira@gmail.com>
2023-04-24 18:59:05 -0300

    adds useEffect to close dropdown when click off happens

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,23 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Dropdown() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
 
   const handleDropdown = () => {
     setIsOpen(!isOpen);
@@ -9,7 +25,7 @@ function Dropdown() {
   
   return (
     <div className="relative inline-block text-left">
-      <div>
+      <div ref={dropdownRef}>
         <button
           type="button"
           className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
@@ -18,7 +34,7 @@ function Dropdown() {
           aria-haspopup="true"
           onClick={handleDropdown}
         >
-          Our Members
+          Developers
           <svg
             className="-mr-1 h-5 w-5 text-gray-400"
             viewBox="0 0 20 20"
@@ -39,7 +55,7 @@ function Dropdown() {
         aria-orientation="vertical"
         aria-labelledby="menu-button"
         tabIndex="-1"
-        hidden={isOpen}
+        hidden={!isOpen}
       >
         <div className="py-1" role="none">
           <a
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Dropdown from "./Dropdown";
 
 function Navbar() {
   return (
-    <nav className="flex justify-around">
+    <nav className="flex justify-around py-[2rem]">
       <span className="font-semibold">SOFTWIZ US</span>
       <Dropdown />
     </nav>
