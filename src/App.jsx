7fff217c0bc3d96a5085e78877cd0ca2feb71ec6
Carlos Vieira <carlepsvieira@gmail.com>
2023-04-24 18:38:52 -0300

    adds dropdown menu with links to members portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>
+    <div>
+      <Navbar />
      <h1>Hello From SoftWizUs</h1>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import Dropdown from "./Dropdown";
+
+function Navbar() {
+  return (
+    <nav>
+      <span className="font-semibold">SOFTWIZ US</span>
+      <Dropdown />
+    </nav>
+  );
+}
+
+export default Navbar;
