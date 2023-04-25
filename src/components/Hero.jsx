294faa1dcd8299fcee01fe46c9396b562380cfa1
Carlos Vieira <carlepsvieira@gmail.com>
2023-04-24 22:51:27 -0300

    adds stack icons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,34 @@
-import 'animate.css';
 import Contact from "./Contact";
+import "animate.css";
+import {
+  SiPython,
+  SiReact,
+  SiRuby,
+  SiVuedotjs,
+  SiPhp,
+  SiJavascript,
+  SiTypescript,
+  SiExpress
+} from "react-icons/si";
 
 function Hero() {
   return (
     <div className="flex justify-around items-center h-screen hero">
-      <div className="flex flex-col gap-4 animate__animated animate__fadeInLeft">
-        <h1 className="text-5xl text-white">As Freelancers,</h1>
-        <h2 className="text-7xl text-white">All About Your Project</h2>
+      <div className="flex flex-col gap-4 animate__animated animate__fadeInLeft text-white">
+        <h1 className="text-5xl">As Freelancers,</h1>
+        <h2 className="text-7xl">All About Your Project</h2>
+        <div className="flex gap-4 mt-[5rem]">
+          <SiReact size={40} />
+          <SiVuedotjs size={40} />
+          <SiJavascript size={40} />
+          <SiTypescript size={40} />
+          <SiExpress size={40}/>
+          <SiPython size={40} />
+          <SiRuby size={40} />
+          <SiPhp size={40} />
+        </div>
       </div>
+      
       <div>
         <Contact />
       </div>
